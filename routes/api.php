dd6effe0d87c05a8f93629baa21bc968dd6c13a5
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2024-02-26 22:05:41 +0500

    admin alias in api

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth', [UserController::class, 'authenticate']);
 
 Route::post('/reg', [UserController::class, 'register']);
+
+Route::group(['middleware' => 'admin'], function() {
+    // routes that should be accessible only by admins
+});
